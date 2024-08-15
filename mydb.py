4bdad7f0145a5f7314850c4763ebbf0dff77b663
imKiri00 <miki.k.biz00@gmail.com>
2024-08-16 01:49:12 +0200

    Initial

---
+++ mydb.py
@@ -0,0 +1,16 @@
+#pdcrm
+import mysql.connector
+
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'password',
+)
+cursorObject = dataBase.cursor()
+
+
+#Create Database
+cursorObject.execute("CREATE DATABASE pdcrm")
+
+print("done")
